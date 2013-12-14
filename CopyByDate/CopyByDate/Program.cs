ac4b830b4da37d79f75598f78c4a81bd6e997bba
Eitan Behar <eitan@baconao.net>
2013-12-14 20:58:14 +0200

    allow copy by filename date

--- CopyByDate/CopyByDate/Program.cs
+++ CopyByDate/CopyByDate/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Reflection;
+using System.Globalization;
 
 namespace CopyByDate
 {
@@ -18,9 +19,24 @@ namespace CopyByDate
                 {
                     continue;
                 }
+
+                DateTime time;
+
+                string fileName = Path.GetFileNameWithoutExtension(file);
+
+                string prefix = "";
+
+                if (fileName.IndexOf("_") == 8)
+                {
+                    prefix = fileName.Substring(0, fileName.IndexOf("_"));
+                    prefix = String.Format("{0}-{1}-{2}", prefix.Substring(0, 4), prefix.Substring(4, 2), prefix.Substring(6, 2));
+                }
                 
-                DateTime time = File.GetCreationTime(file);
-                
+                if (!DateTime.TryParse(prefix, out time))
+                {
+                    time = File.GetCreationTime(file);
+                }
+
                 string folderName = rootPath + "\\" + time.Year.ToString() + "_" + time.Month.ToString() + "_" + time.Day.ToString();
 
                 if (!Directory.Exists(folderName))
