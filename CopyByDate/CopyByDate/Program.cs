db7b700d8290532437ce9c57ffe352f885f71425
Eitan Behar <eitan@baconao.net>
2013-12-14 20:28:18 +0200

    adding files

---
+++ CopyByDate/CopyByDate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace CopyByDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            foreach (string file in Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly))
+            {
+                if (String.Compare(file, Assembly.GetExecutingAssembly().Location, true) == 0)
+                {
+                    continue;
+                }
+                
+                DateTime time = File.GetCreationTime(file);
+                
+                string folderName = rootPath + "\\" + time.Year.ToString() + "_" + time.Month.ToString() + "_" + time.Day.ToString();
+
+                if (!Directory.Exists(folderName))
+                {
+                    Directory.CreateDirectory(folderName);
+                }
+
+                try
+                {
+                    File.Move(file, Path.Combine(folderName, Path.GetFileName(file)));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Cannot move: " + file + " due to: " + ex.Message);
+                }
+            }
+        }
+    }
+}
